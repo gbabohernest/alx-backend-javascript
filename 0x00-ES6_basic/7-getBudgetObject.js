1d54582b403b8d60fb84f57c32aa0bff1430e165
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-05 02:55:04 +0000

    object property value shortend sytanx

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,6 @@
+export default function getBudgetObject(income, gdp, capita) {
+  // Using ES6 shorthand property syntax to simplify object creation
+  const budget = { income, gdp, capita };
+
+  return budget;
+}
