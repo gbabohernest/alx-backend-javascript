d7c0a1a629f2b0b47624854ea05bdc8dc039337d
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-05 15:43:34 +0000

    object property value shorthand syntax

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,10 @@
-export default (income, gdp, capita) => ({
-  income,
-  gdp,
-  capita,
-});
+export default function getBudgetObject(income, gdp, capita) {
+  // Using ES6 shorthand property syntax to simplify object creation
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
