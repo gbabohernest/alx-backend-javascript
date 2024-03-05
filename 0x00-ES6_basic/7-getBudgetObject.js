77a879c843234168f47247081fa82e5852b56c72
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-05 15:36:20 +0000

    fix correct output error

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,6 +1,5 @@
-export default function getBudgetObject(income, gdp, capita) {
-  // Using ES6 shorthand property syntax to simplify object creation
-  const budget = { income, gdp, capita };
-
-  return budget;
-}
+export default (income, gdp, capita) => ({
+  income,
+  gdp,
+  capita,
+});
