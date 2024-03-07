6e10227ee411b189e9d9c9405ba889191947715b
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-07 02:53:14 +0000

    use asyn & await to handle the return promise form the promise.allSettled method and for correct output of code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  filename,
+) {
   const signUpUserPromiseObject = signUpUser(firstName, lastName);
   const uploadPhotoPromiseObject = uploadPhoto(filename);
 
-  return Promise.allSettled([
+  const result = await Promise.allSettled([
     signUpUserPromiseObject,
     uploadPhotoPromiseObject,
-  ]).then((data) => data.map((element) => ({
+  ]);
+
+  return result.map((element) => ({
     status: element.status,
     value: element.status === 'fulfilled' ? element.value : element.reason,
-  })));
+  }));
 }
