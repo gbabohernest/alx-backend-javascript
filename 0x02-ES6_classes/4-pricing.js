f38076665d2eaa46637824cc1d2a3a0c363fb4a1
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-08 04:55:16 +0000

    class Pricing, implements getter and setter for each attribute (number, currency). implement a method that returns price amount and the currency name along with the currency code

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,49 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    // perform some checks on the values passed when instantiating the class
+    if (typeof amount !== 'number' || !(currency instanceof Currency)) {
+      throw new Error('Values are invalid');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (value !== 'number') {
+      throw new Error('Amount must be a number');
+    }
+    this._amount = value;
+    return value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(objValue) {
+    if (!(objValue instanceof Currency)) {
+      throw new Error('currency must be an instance of Currency');
+    }
+    this._currency = objValue;
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static covertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new Error('Type must be a number');
+    }
+
+    const price = amount * conversionRate;
+    return price;
+  }
+}
