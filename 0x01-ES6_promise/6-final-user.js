71dfdef7599bf824176608dce4d7a32bc6a00275
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-09 01:03:59 +0000

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  filename,
-) {
-  const signUpUserPromiseObject = signUpUser(firstName, lastName);
-  const uploadPhotoPromiseObject = uploadPhoto(filename);
-
-  const result = await Promise.allSettled([
-    signUpUserPromiseObject,
-    uploadPhotoPromiseObject,
-  ]);
-
-  return result.map((element) => ({
-    status: element.status,
-    value: element.status === 'fulfilled' ? element.value : element.reason,
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename),
+  ]).then((data) => data.map((element) => {
+    if (element.status === 'rejected') {
+      return { status: 'rejected', value: element.message };
+    }
+    return { status: 'fulfilled', value: element };
   }));
 }
