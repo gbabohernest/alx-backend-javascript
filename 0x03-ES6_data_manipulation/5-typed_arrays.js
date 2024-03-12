8a8410a8798448e53a6033a0f34e889d4c39cc38
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-12 05:28:17 +0000

    Typed Array, a function that returns a new ArrayBufer with an Int8 value at a specific position

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default (length, position, value) => {
+  if (position < 0 || position >= length) {
+    throw new Error('Position is out of bonds');
+  }
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+  int8View[position] = value;
+  return new DataView(buffer);
+};
