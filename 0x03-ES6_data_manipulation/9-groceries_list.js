8efaa0be87e3d9696f3eecb15e2202175fb7bb08
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-13 00:57:46 +0000

    properly set the item and quantity in the Map object using set and retun the map object

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,7 +1,15 @@
-export default () => new Map([
-  ['Apple', 10],
-  ['Tomatoes', 10],
-  ['Pasta', 1],
-  ['Rice', 1],
-  ['Banana', 5],
-]);
+export default () => {
+  const groceriesListMap = new Map();
+  const groceryItems = {
+    Apple: 10,
+    Tomatoes: 10,
+    Pasta: 1,
+    Rice: 1,
+    Banana: 5,
+  };
+
+  for (const [name, quantity] of Object.entries(groceryItems)) {
+    groceriesListMap.set(name, quantity);
+  }
+  return groceriesListMap;
+};
