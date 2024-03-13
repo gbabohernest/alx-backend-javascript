0ef722c35b9825c3af97fa7147eb602528b23635
Ernest P. Gbaboh <gbaboh.ernest@gmail.com>
2024-03-13 05:57:47 +0300

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default (length, position, value) => {
-  if (position >= length) {
-    throw new Error('Position is out of bonds');
+  if (position < 0 || position >= length) {
+      throw new Error('Position is out of bonds');
   }
-  
-  const buffer = new DataView(new ArrayBuffer(length), 0, length);
-  buffer = setUint8(position, value);
-  return buffer;
-};
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+  int8View[position] = value;
+  return new DataView(buffer);
+}
