1dee5a074d02d957f8d6af3cd1273f65a2f5038f
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-13 03:03:52 +0000

    typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,13 @@
 export default (length, position, value) => {
+  if (typeof length !== 'number' || typeof position !== 'number' || typeof value !== 'number') {
+    throw new Error('Invalid type');
+  }
+
   if (position < 0 || position >= length) {
-      throw new Error('Position is out of bonds');
+    throw new Error('Position is out of bonds');
   }
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer);
   int8View[position] = value;
   return new DataView(buffer);
-}
+};
