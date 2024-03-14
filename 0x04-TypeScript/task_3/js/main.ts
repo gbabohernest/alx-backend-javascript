844560fbb0825d15e26dae2944eb71ad8f17ec9e
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-03-14 05:34:57 +0300

    ambient namespaces

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,30 @@
+/// <reference path="./crud.d.ts" />
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+const updateRow: RowElement = {
+  ...row,
+  age: 23,
+};
+
+CRUD.updateRow(newRowID, updateRow);
+CRUD.deleteRow(newRowID);
+
+// testing
+/* const obj = { firstName: 'Guillaume', lastName: 'Salva' };
+CRUD.insertRow(obj);
+
+const updatedRow: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+  age: 23,
+};
+CRUD.updateRow(newRowID, updatedRow);
+
+CRUD.deleteRow(125);
+ */
