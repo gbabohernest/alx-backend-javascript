723a84b6bb9fb067cb589088da3ea87b38e6d746
gbabohernest <gbaboh.ernest@gmail.com>
2024-04-30 23:11:55 +0300

    Refactor to handle correct output error

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Listen for user input on stdin
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  if (input) {
-    console.log(`Your name is: ${input}`);
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input != null) {
+    const userInput = input.toString().trim();
+    process.stdout.write(`Your name is: ${userInput}\n`);
   }
-
-  console.log('This important software is now closing');
-  process.exit(0);
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
