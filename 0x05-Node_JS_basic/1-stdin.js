8f1b1faed78afee611f668c4cc67f9f52ff74797
gbabohernest <gbaboh.ernest@gmail.com>
2024-04-30 23:23:07 +0300

    correct output error fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,9 +3,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 // Listen for user input on stdin
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  if (input != null) {
-    const userInput = input.toString().trim();
-    process.stdout.write(`Your name is: ${userInput}\n`);
+  if (input) {
+    // const userInput = input.toString().trim();
+    process.stdout.write(`Your name is: ${input}`);
   }
 });
 process.stdin.on('end', () => {
