470925283e8827d595086eb4ebdc9df794083baa
gbabohernest <gbaboh.ernest@gmail.com>
2024-05-02 21:44:00 +0300

    Spies

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,26 @@
+const Utils = {
+    calculateNumber(type, a, b) {
+        const roundedA = Math.round(a);
+        const roundedB = Math.round(b);
+
+        const typeValue = type.toUpperCase();
+        switch (typeValue) {
+            case 'SUM':
+                return roundedA + roundedB;
+
+            case 'SUBTRACT':
+                return roundedA - roundedB;
+
+            case 'DIVIDE':
+                if (roundedB === 0) {
+                    return 'Error';
+                }
+                return roundedA / roundedB;
+
+            default:
+                throw new Error('Unknown operation');
+        }
+    }
+};
+
+module.exports = Utils;
