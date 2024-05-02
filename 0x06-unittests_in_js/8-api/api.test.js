5fe63e215674232f005cf3b062ae9e013b043c81
gbabohernest <gbaboh.ernest@gmail.com>
2024-05-03 00:35:57 +0300

    update, fix eslint error

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,19 +1,20 @@
+/* eslint-disable no-unused-vars */
 const request = require('request');
 const { expect } = require('chai');
-const server = require('./api');
+// const server = require('./api');
 
-describe('index page', () => {
-  it('should return correct status code', () => new Promise((done) => {
+describe('Index page', () => {
+  it('should return correct status code', () => (done) => {
     request.get('http://localhost:7865', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
+      expect(response.statusCode).to.be.equal(200);
       done();
     });
-  }));
+  });
 
-  it('should return correct result', () => new Promise((done) => {
+  it('should return correct result', () => (done) => {
     request.get('http://localhost:7865', (error, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
-  }));
+  });
 });
